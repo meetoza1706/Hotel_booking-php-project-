74703a0427b01adfb7a3d0cfe8912aca3faea40e
Meet Oza <ozamee17@gmail.com>
2024-04-03 00:07:30 +0530

    all files added

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "hello world!";
+?><!DOCTYPE html>
+<html>
+    <head>
+        <title>Hotel Booking system</title>
+    </head>
+    <body>
+        <h2>Hotel Booking System</h2>
+        <nav>
+            
+        </nav>
+    </body>
+</html>
