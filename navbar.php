cafe8637630396544b1401f230abfd000ac918ff
Meet Oza <ozamee17@gmail.com>
2024-04-03 01:16:48 +0530

    day 1 over

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WiseStay</title>
+        <link rel="stylesheet" href="navbar.css">
+    </head>
+    <body>
+        <nav class="navbar">
+        <img class="logo-img" src="logo.png">
+        <a class="home" href="#">Home</a>
+        <a class="contact_us" href="#">contact us</a>
+        <a class="about_us" href="#">about us</a>
+        </nav>
+    </body>
+</html>
