baaed972e18bc6284431cf1103bdd12c6ad73f9a
Meet Oza <ozamee17@gmail.com>
2024-04-10 18:03:13 +0530

    10th april

--- index.php
+++ index.php
@@ -10,7 +10,20 @@
         include 'navbar.php';
         ?>
 
-    <div class="heading"><h1 class="heading_text">Hotel Booking System</h1></div>
+    <div class="heading"><h1 class="heading_text">Hotel Booking System</h1></div>   
+
+    <div class="search_container">
+        <div class="city"><input type="text"></div>
+        <div class="inn_date"></div> <!--check_inn date-->
+
+
+
+
+
+        <div class="out_date">check-out</div><!--check_out Date-->
+        <div class="person">person</div> <!--adults and kids-->
+        <button class="search-btn">Search</button>
+    </div>
         
     </body>
 </html>
